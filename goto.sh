48342325a1af072f099f3529417760c95d89f146
difki <i.difki@gmail.com>
2017-02-02 08:53:58 +0200

    chmod + scritp

--- goto.sh
+++ goto.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 set -m # Enable Job Control
-
-for i in `seq $1`; do 
-  echo $i &
+num_of_runs=$2
+site=$1
+for run in `seq $num_of_runs`; do 
+	echo "Run - $run";
+	curl -s $site > /dev/null  &
+	echo "Rinning pid - $!"
 done
 
+
 # Wait for all parallel jobs to finish
-while [ 1 ]; do fg 2> /dev/null; [ $? == 1 ] && break; done
+#while [ 1 ]; do fg 2> /dev/null; [ $? == 1 ] && break; done
